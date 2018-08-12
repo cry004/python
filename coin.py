95cd4289bdbdf8ec9433615bdeac381486b3e069
cry004 <cry004@gmail.com>
2018-08-12 11:05:50 +0800

    coin object finish

--- coin.py
+++ coin.py
@@ -1,6 +1,10 @@
 import random
 class Coin:
     def __init__(self,rare=False,clean=True, heads = True, **kwargs):
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            self.original_value = 1.00
         self.is_rare = rare
         self.is_clean = clean
         self.heads = heads
@@ -21,24 +25,116 @@ class Coin:
             self.colour = self.clean_colour
 
         def __del__(self):
-        print("Coin Spent!")
+            print("Coin Spent!")
 
         def flip(self):
-        options = [True,False]
-        choice = random.choice(options)
-        self.heads = choice
+            options = [True,False]
+            choice = random.choice(options)
+            self.heads = choice
+            
+        def __str__(self):
+            if self.original_value > 1.00:
+                return "${} Coin".format(int(self.original_value))
+            else:
+                return "{}p Coin".format(int(self.original_value*100))
+    
+class One_pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.01,
+            "clean_colour": "bronze",
+            "rusty_colour": "brownish",
+            "num_edges": 1,
+            "diameter": 20.5,
+            "thickness": 1.52,
+            "mass": 3.56
+            }
+        super().__init__(**data)
 
-        
+class Two_pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.02,
+            "clean_colour": "bronze",
+            "rusty_colour": "brownish",
+            "num_edges": 1,
+            "diameter": 25.9,
+            "thickness": 1.85,
+            "mass": 7.12
+            }
+        super().__init__(**data)
+
+class Three_pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.05,
+            "clean_colour": "silver",
+            "rusty_colour": None,
+            "num_edges": 1,
+            "diameter":18,
+            "thickness": 1.77,
+            "mass": 3.25
+        }
+        super().__init__(**data)
+
+    def rust(self):
+        self.colour = self.clean_colour
+    def clean(self):
+        self.colour = self,clean_colour
+
+class Ten_pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.05,
+            "clean_colour": "silver",
+            "rusty_colour": None,
+            "num_edges": 1,
+            "diameter":18,
+            "thickness": 1.77,
+            "mass": 3.25
+        }
+        super().__init__(**data)
+
+    def rust(self):
+        self.colour = self.clean_colour
+    def clean(self):
+        self.colour = self,clean_colour
+
+class Twenty_pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.05,
+            "clean_colour": "silver",
+            "rusty_colour": None,
+            "num_edges": 1,
+            "diameter":18,
+            "thickness": 1.77,
+            "mass": 3.25
+        }
+        super().__init__(**data)
+
+    def rust(self):
+        self.colour = self.clean_colour
+    def clean(self):
+        self.colour = self,clean_colour 
+
+coins = [One_pence(), Two_pence(),Three_pence(),Ten_pence(),Twenty_pence()]
+
+for coin in coins:
+    arguments = [coin, coin.colour, coin.value, coin.diameter, coin.thickness, coin.num_edges, coin.mass]
+    string = "{} - Colour: {}, value: {}, diameter(mm): {},thickness(mm): {}, number of edges: {}, mass(g): {}" .format(*arguments)        
+    print(string)
 class Pound(Coin):
     def __init__(self):
         data = {
             "original_value": 1.00,
             "clean_colour": "gold",
-            "rusty_colour": "greenish".
+            "rusty_colour": "greenish",
             "num_edges": 1,
             "diameter": 22.5,
             "thickness": 3.15,
             "mass": 9.5
+            }
         super().__init__(**data)
     
 #      def __init__(self, rare=False):
